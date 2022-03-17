958e9785f5d9ed399c9feda4587b7ea8babe8659
Mitch Chanza <mitch.chanza@gmail.com>
2022-03-18 01:19:34 +0200

    aptude

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const main_1 = require("./main");
+/** initialize the app **/
+(0, main_1.writeVersion)().then(r => console.log(r));
+//# sourceMappingURL=index.js.map
---
+++ src/main.js
@@ -0,0 +1,81 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.writeVersion = void 0;
+const pv = __importStar(require("../package.json"));
+const exec = __importStar(require("child_process"));
+const core = __importStar(require("@actions/core"));
+const node_version = pv.version;
+const numbers = pv.version.split(".");
+let { first, second, last } = {
+    first: parseInt(numbers[0]),
+    second: parseInt(numbers[1]),
+    last: parseInt(numbers[2])
+};
+if (last < 9) {
+    last += 1;
+}
+else if (last >= 9) {
+    last = 0;
+    run_second();
+}
+function run_second() {
+    if (second < 9) {
+        second += 1;
+    }
+    else if (second >= 9) {
+        second = 0;
+        first += 1;
+    }
+}
+const newVersion = first + "." + second + "." + last;
+function writeVersion() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const override = core.getInput('override');
+            // @ts-ignore
+            yield exec(`sed -i "s/${node_version}/${newVersion}/g" package.json`, ((error, stdout, stderr) => {
+                if (error)
+                    console.log(`stdout:${error}`);
+                if (stderr)
+                    console.log(`stderr: ${stderr}`);
+                console.log(`increased version", ${newVersion}  with an overide ${override}`);
+            }));
+        }
+        catch (err) {
+        }
+    });
+}
+exports.writeVersion = writeVersion;
+//# sourceMappingURL=main.js.map
